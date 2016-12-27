a998a0f5fc6be8c6151fbbea743d20f154e7d27b
Radium Zheng <ztl8702@msn.com>
2016-12-27 15:46:19 +1100

    save

--- src/CrowdSource/Controllers/TestController.cs
+++ src/CrowdSource/Controllers/TestController.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CrowdSource.Controllers
 {
     public class TestController : Controller
     {
+        private ILogger<TestController> _logger;
+        public TestController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TestController>();
+        }
         public IActionResult Index()
         {
             return View();
         }
 
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitToDo(int gid)
+        {
+            _logger.LogInformation($"{gid} submitted");
+            return View("List");
+        }
 
     }
 
