78ec8e5b57db81435882c6364347ab8b517d45c9
Radium Zheng <ztl8702@msn.com>
2016-12-31 13:25:37 +1100

    BanJiao

---
+++ src/CrowdSource/Services/TextSanitizer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdSource.Services
+{
+    public class TextSanitizer : ITextSantizer
+    {
+        //http://www.cnblogs.com/songtzu/archive/2012/08/07/2627239.html
+        public string BanJiao(string input)
+        {
+            char[] cc = input.ToCharArray();
+            for (int i = 0; i < cc.Length; i++)
+            {
+                if (cc[i] == 12288)
+                {
+                    // 表示空格  
+                    cc[i] = (char)32;
+                    continue;
+                }
+                if (cc[i] > 65280 && cc[i] < 65375)
+                {
+                    cc[i] = (char)(cc[i] - 65248);
+                }
+
+            }
+            return new string(cc);
+        }
+    }
+
+    public interface ITextSantizer
+    {
+        string BanJiao(string input);
+    }
+}
--- test/CrowdSource.Test/Tests.cs
+++ test/CrowdSource.Test/Tests.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using CrowdSource.Services;
 
 namespace Tests
 {
@@ -9,6 +10,20 @@ namespace Tests
         public void Test1()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void TestBanJiao()
+        {
+
+            Assert.Equal(new TextSanitizer().BanJiao("abc（），；"), "abc(),;");
+            // Unicode extended
+            Assert.Equal(new TextSanitizer().BanJiao("abc（）??"), "abc()??");
+            // BUC
+            Assert.Equal(new TextSanitizer().BanJiao("abc（）??ng??ngé??ngé??k??ngê??ng??k"), "abc()??ng??ngé??ngé??k??ngê??ng??k");
+            // 中文句号不会变成句点
+            Assert.Equal(new TextSanitizer().BanJiao("。"), "。");
+
         }
     }
 }
