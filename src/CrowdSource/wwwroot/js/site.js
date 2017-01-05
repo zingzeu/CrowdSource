3ec76a06c2af8dba7d8cfd34dded1217633cfd4f
Radium Zheng <ztl8702@msn.com>
2017-01-06 04:42:33 +1100

    EditGroup View Page

--- src/CrowdSource/wwwroot/js/site.js
+++ src/CrowdSource/wwwroot/js/site.js
@@ -1,1 +1,19 @@
 ﻿// Write your Javascript code.
+
+$(document).ready(function () {
+
+    if ($('#IsPivotRow').is(":checked")) {
+        $("#PivotRowSpecificFields").show();
+    } else {
+        $("#PivotRowSpecificFields").hide();
+    }
+
+    $('#IsPivotRow').click(function () {
+        if ($(this).is(':checked')) {
+            $("#PivotRowSpecificFields").show();
+        } else {
+            $("#PivotRowSpecificFields").hide();
+        }
+    }); 
+});
+
