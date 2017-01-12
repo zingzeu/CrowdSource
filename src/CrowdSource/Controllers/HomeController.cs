74e3cdb9fc59559c5ae4ac507cf2a399ae9486f3
Radium Zheng <ztl8702@msn.com>
2017-01-13 05:53:28 +1100

    Cache Statistics

--- src/CrowdSource/Controllers/HomeController.cs
+++ src/CrowdSource/Controllers/HomeController.cs
@@ -11,41 +11,20 @@ namespace CrowdSource.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ITaskDispatcher _taskDispacher;
-        private readonly ApplicationDbContext _context;
-        public HomeController(ITaskDispatcher taskDispatcher, ApplicationDbContext context)
+        private readonly IAnalytics _analytics;
+        public HomeController(IAnalytics analytics)
         {
-            _taskDispacher = taskDispatcher;
-            _context = context;
+            _analytics = analytics;
         }
+
         public async Task<IActionResult> Index()
         {
-            int total = _context.Groups.Where(g => g.Collection.CollectionId == 1).Count(); 
-            int todo = _taskDispacher.CountToDo();
-            int toreview = _taskDispacher.CountToReview();
-            int done = _context
-                    .Groups
-                    .FromSql("SELECT * FROM \"Groups\" AS \"gg\" \n" +
-                    "WHERE\n" +
-                    "(SELECT COUNT(DISTINCT \"FieldTypes\".\"Name\") FROM\n" +
-                    "  \"GVSuggestions\"\n" +
-                    "   INNER JOIN \"GroupVersions\" ON \"GVSuggestions\".\"GroupVersionForeignKey\" = \"GroupVersions\".\"GroupVersionId\"\n" +
-                    "   INNER JOIN \"FieldTypes\" ON \"GVSuggestions\".\"FieldTypeForeignKey\" = \"FieldTypes\".\"FieldTypeId\"\n" +
-                    " WHERE \"GroupVersions\".\"GroupId\" = \"gg\".\"GroupId\"\n" +
-                    " AND \"FieldTypes\".\"Name\" IN('TextBUC', 'TextEnglish', 'TextChinese')\n" +
-                    " AND \"GroupVersions\".\"NextVersionGroupVersionId\" IS NULL\n" +
-                    ") >= 3\n" //罗 英 中 全
-                    )
-                    .Count();
-            int reviewed = done - toreview;
-            int percentDone = (int)done * 100 / total;
-            int percentReviewd = (int)reviewed * 100 / done;
-            done = done > 0 ? done : 0;
-            reviewed = reviewed > 0 ? reviewed : 0;
-            ViewData["ToDoTotal"] = total;
-            ViewData["Done"] = done;
-            ViewData["ReviewTotal"] = done;
-            ViewData["Reviewed"] = reviewed;
+            ViewData["ToDoTotal"] = _analytics.ToDoTotal;
+            ViewData["Done"] = _analytics.Done;
+            ViewData["ReviewTotal"] = _analytics.ReviewTotal;
+            ViewData["Reviewed"] = _analytics.Reviewed;
+            int percentDone = (int)_analytics.Done * 100 / _analytics.ToDoTotal;
+            int percentReviewd = (int)_analytics.Reviewed * 100 / _analytics.ReviewTotal;
             ViewData["DonePercent"] = percentDone;
             ViewData["ReviewPercent"] = percentReviewd;
             return View();
---
+++ src/CrowdSource/Services/Analytics.cs
@@ -0,0 +1,74 @@
+﻿using CrowdSource.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrowdSource.Services
+{
+    public class Analytics : IAnalytics
+    {
+        private readonly ITaskDispatcher _taskDispacher;
+        private readonly ApplicationDbContext _context;
+        private readonly Timer aTimer;
+
+        public int ToDoTotal { get; private set; }
+
+        public int Done { get; private set; }
+
+        public int ReviewTotal { get; private set; }
+
+        public int Reviewed { get; private set; }
+
+        public Analytics(ITaskDispatcher taskDispatcher, ApplicationDbContext context)
+        {
+            _taskDispacher = taskDispatcher;
+            _context = context;
+
+            UpdateStatistics().Wait();
+
+            aTimer = new Timer(async a => {
+               await UpdateStatistics();
+            },null,0,5*60*1000); //5 min
+        }
+
+        private async Task UpdateStatistics()
+        {
+            int total = _context.Groups.Where(g => g.Collection.CollectionId == 1).Count();
+            int todo = _taskDispacher.CountToDo();
+            int toreview = _taskDispacher.CountToReview();
+            int done = await _context
+                    .Groups
+                    .FromSql("SELECT * FROM \"Groups\" AS \"gg\" \n" +
+                    "WHERE\n" +
+                    "(SELECT COUNT(DISTINCT \"FieldTypes\".\"Name\") FROM\n" +
+                    "  \"GVSuggestions\"\n" +
+                    "   INNER JOIN \"GroupVersions\" ON \"GVSuggestions\".\"GroupVersionForeignKey\" = \"GroupVersions\".\"GroupVersionId\"\n" +
+                    "   INNER JOIN \"FieldTypes\" ON \"GVSuggestions\".\"FieldTypeForeignKey\" = \"FieldTypes\".\"FieldTypeId\"\n" +
+                    " WHERE \"GroupVersions\".\"GroupId\" = \"gg\".\"GroupId\"\n" +
+                    " AND \"FieldTypes\".\"Name\" IN('TextBUC', 'TextEnglish', 'TextChinese')\n" +
+                    " AND \"GroupVersions\".\"NextVersionGroupVersionId\" IS NULL\n" +
+                    ") >= 3\n" //罗 英 中 全
+                    )
+                    .CountAsync();
+            int reviewed = done - toreview;
+
+            done = done > 0 ? done : 0;
+            reviewed = reviewed > 0 ? reviewed : 0;
+            ToDoTotal = total;
+            Done = done;
+            ReviewTotal = done;
+            Reviewed = reviewed;
+        }
+    }
+
+    public interface IAnalytics
+    {
+        int ToDoTotal { get; }
+        int Done { get; }
+        int ReviewTotal { get; }
+        int Reviewed { get;  }
+    }
+}
