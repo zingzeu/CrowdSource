55e995dfd63b10a03a708e91b52cf5bcfb9a65d7
Radium Zheng <ztl8702@msn.com>
2017-01-15 23:15:37 +1100

    Detailed progress. shorten progress refresh time.

--- src/CrowdSource/Controllers/HomeController.cs
+++ src/CrowdSource/Controllers/HomeController.cs
@@ -21,11 +21,26 @@ namespace CrowdSource.Controllers
         {
             ViewData["ToDoTotal"] = _analytics.ToDoTotal;
             ViewData["Done"] = _analytics.Done;
+            
+            ViewData["DoneBUC"] = _analytics.DoneBUC;
+            ViewData["DoneEnglish"] = _analytics.DoneEnglish;
+            ViewData["DoneChinese"] = _analytics.DoneChinese;
+            
             ViewData["ReviewTotal"] = _analytics.ReviewTotal;
             ViewData["Reviewed"] = _analytics.Reviewed;
             int percentDone = _analytics.ToDoTotal>0 ? (int)_analytics.Done * 100 / _analytics.ToDoTotal : 0;
+            
+            int percentDoneBUC = _analytics.ToDoTotal>0 ? (int)_analytics.DoneBUC * 100 / _analytics.ToDoTotal : 0;
+            int percentDoneChinese = _analytics.ToDoTotal>0 ? (int)_analytics.DoneChinese * 100 / _analytics.ToDoTotal : 0;
+            int percentDoneEnglish = _analytics.ToDoTotal>0 ? (int)_analytics.DoneEnglish * 100 / _analytics.ToDoTotal : 0;
+            
             int percentReviewd = _analytics.ReviewTotal>0 ? (int)_analytics.Reviewed * 100 / _analytics.ReviewTotal : 0;
             ViewData["DonePercent"] = percentDone;
+
+            ViewData["DonePercentBUC"] = percentDoneBUC;
+            ViewData["DonePercentEnglish"] = percentDoneEnglish;
+            ViewData["DonePercentChinese"] = percentDoneChinese;
+            
             ViewData["ReviewPercent"] = percentReviewd;
             return View();
         }
--- src/CrowdSource/Services/Analytics.cs
+++ src/CrowdSource/Services/Analytics.cs
@@ -17,6 +17,10 @@ namespace CrowdSource.Services
         public int ToDoTotal { get; private set; }
 
         public int Done { get; private set; }
+        
+        public int DoneBUC { get; private set; }
+        public int DoneEnglish { get; private set; }
+        public int DoneChinese { get; private set; }
 
         public int ReviewTotal { get; private set; }
 
@@ -31,7 +35,7 @@ namespace CrowdSource.Services
 
             aTimer = new Timer(async a => {
                await UpdateStatistics();
-            },null,0,5*60*1000); //5 min
+            },null,0,1*60*1000); //1 min
         }
 
         private async Task UpdateStatistics()
@@ -53,12 +57,57 @@ namespace CrowdSource.Services
                     ") >= 3\n" //罗 英 中 全
                     )
                     .CountAsync();
+            int doneBUC = await _context
+                    .Groups
+                    .FromSql("SELECT * FROM \"Groups\" AS \"gg\" \n" +
+                    "WHERE\n" +
+                    "(SELECT COUNT(DISTINCT \"FieldTypes\".\"Name\") FROM\n" +
+                    "  \"GVSuggestions\"\n" +
+                    "   INNER JOIN \"GroupVersions\" ON \"GVSuggestions\".\"GroupVersionForeignKey\" = \"GroupVersions\".\"GroupVersionId\"\n" +
+                    "   INNER JOIN \"FieldTypes\" ON \"GVSuggestions\".\"FieldTypeForeignKey\" = \"FieldTypes\".\"FieldTypeId\"\n" +
+                    " WHERE \"GroupVersions\".\"GroupId\" = \"gg\".\"GroupId\"\n" +
+                    " AND \"FieldTypes\".\"Name\" IN('TextBUC')\n" +
+                    " AND \"GroupVersions\".\"NextVersionGroupVersionId\" IS NULL\n" +
+                    ") >= 1\n" //罗
+                    )
+                    .CountAsync();
+            int doneChinese = await _context
+                    .Groups
+                    .FromSql("SELECT * FROM \"Groups\" AS \"gg\" \n" +
+                    "WHERE\n" +
+                    "(SELECT COUNT(DISTINCT \"FieldTypes\".\"Name\") FROM\n" +
+                    "  \"GVSuggestions\"\n" +
+                    "   INNER JOIN \"GroupVersions\" ON \"GVSuggestions\".\"GroupVersionForeignKey\" = \"GroupVersions\".\"GroupVersionId\"\n" +
+                    "   INNER JOIN \"FieldTypes\" ON \"GVSuggestions\".\"FieldTypeForeignKey\" = \"FieldTypes\".\"FieldTypeId\"\n" +
+                    " WHERE \"GroupVersions\".\"GroupId\" = \"gg\".\"GroupId\"\n" +
+                    " AND \"FieldTypes\".\"Name\" IN('TextChinese')\n" +
+                    " AND \"GroupVersions\".\"NextVersionGroupVersionId\" IS NULL\n" +
+                    ") >= 1\n" //中
+                    )
+                    .CountAsync();
+            int doneEnglish = await _context
+                    .Groups
+                    .FromSql("SELECT * FROM \"Groups\" AS \"gg\" \n" +
+                    "WHERE\n" +
+                    "(SELECT COUNT(DISTINCT \"FieldTypes\".\"Name\") FROM\n" +
+                    "  \"GVSuggestions\"\n" +
+                    "   INNER JOIN \"GroupVersions\" ON \"GVSuggestions\".\"GroupVersionForeignKey\" = \"GroupVersions\".\"GroupVersionId\"\n" +
+                    "   INNER JOIN \"FieldTypes\" ON \"GVSuggestions\".\"FieldTypeForeignKey\" = \"FieldTypes\".\"FieldTypeId\"\n" +
+                    " WHERE \"GroupVersions\".\"GroupId\" = \"gg\".\"GroupId\"\n" +
+                    " AND \"FieldTypes\".\"Name\" IN('TextEnglish')\n" +
+                    " AND \"GroupVersions\".\"NextVersionGroupVersionId\" IS NULL\n" +
+                    ") >= 1\n" //英
+                    )
+                    .CountAsync();
             int reviewed = done - toreview;
 
             done = done > 0 ? done : 0;
             reviewed = reviewed > 0 ? reviewed : 0;
             ToDoTotal = total;
             Done = done;
+            DoneBUC = doneBUC;
+            DoneChinese = doneChinese;
+            DoneEnglish = doneEnglish;
             ReviewTotal = done;
             Reviewed = reviewed;
         }
@@ -68,6 +117,9 @@ namespace CrowdSource.Services
     {
         int ToDoTotal { get; }
         int Done { get; }
+        int DoneBUC { get; }
+        int DoneEnglish { get; }
+        int DoneChinese { get; }
         int ReviewTotal { get; }
         int Reviewed { get;  }
     }
