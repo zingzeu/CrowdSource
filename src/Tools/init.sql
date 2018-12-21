b3f9db996623c9225f6e62efe527e15347025682
Radium Zheng <ztl8702@msn.com>
2018-12-22 04:11:53 +1100

    remove old code

--- src/Tools/init.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "Collections" VALUES (DEFAULT, 'ADFD');
-
-INSERT INTO "Groups" VALUES 
-(DEFAULT,1,NULL, '{"ImgFileName":"page_0028_line_01.png"}'::jsonb),
-(DEFAULT,1,NULL, '{"ImgFileName":"page_0028_line_02.png"}'::jsonb),
-(DEFAULT,1,NULL, '{"ImgFileName":"page_0028_line_03.png"}'::jsonb),
-(DEFAULT,1,NULL, '{"ImgFileName":"page_0028_line_04.png"}'::jsonb);
