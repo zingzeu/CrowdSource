69e03b8cdba250e7c8e7acc0174f7c2f71a35fd4
Radium Zheng <ztl8702@msn.com>
2018-12-29 10:11:45 +1100

    clean up StepGrainData

--- src/Zezo.Core.GrainInterfaces/StepGrainData.cs
+++ src/Zezo.Core.GrainInterfaces/StepGrainData.cs
@@ -8,16 +8,17 @@ namespace Zezo.Core.GrainInterfaces
     public class StepGrainData
     {
         public StepStatus Status { get; set; } = StepStatus.Uninitialized;
-        public string Type { get; set; }
         public string Id { get; set; }
         public Guid? ParentNode { get; set; }
         public Guid Entity {get; set; }
-        public List<Guid> ChildNodes { get; set; } = new List<Guid>();
+        public List<Guid> ChildNodes { get; } = new List<Guid>();
         public int ChildCount => ChildNodes.Count;
         public Dictionary<string, object> Data = new Dictionary<string, object>();
         public StepNode Config { get; set; }
         
-        public HashSet<IStepGrainObserver> Observers = new HashSet<IStepGrainObserver>();
-        
+        /// <summary>
+        /// Client-side observers. (For test only).
+        /// </summary>
+        public HashSet<IStepGrainObserver> Observers { get; } = new HashSet<IStepGrainObserver>();
     }
 }
