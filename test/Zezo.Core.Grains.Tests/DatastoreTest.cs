b9d978ee510846df876d8134b558afc675c26d85
Radium Zheng <ztl8702@msn.com>
2018-12-30 12:40:45 +1100

    wrap test in timeout

--- test/Zezo.Core.Grains.Tests/DatastoreTest.cs
+++ test/Zezo.Core.Grains.Tests/DatastoreTest.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Polly;
+using Polly.Timeout;
 using Xunit;
 using Xunit.Abstractions;
 using Zezo.Core.Configuration;
@@ -13,7 +17,13 @@ namespace Zezo.Core.Grains.Tests
         }
 
         [Fact]
-        public async Task Can_Create_Entity_with_Datastore()
+        public Task Can_Create_Entity_with_Datastore()
+        {
+            return Policy.TimeoutAsync(60, TimeoutStrategy.Pessimistic)
+                .ExecuteAsync(DO_Can_Create_Entity_with_Datastore);
+        }
+        
+        private async Task DO_Can_Create_Entity_with_Datastore()
         {
             var config = ParseConfig(@"
                 <Project Id=""test"">
@@ -42,11 +52,15 @@ namespace Zezo.Core.Grains.Tests
                     </Project.Datastores>
                 </Project>
             ") as ProjectNode;
-
-            var e1 = await CreateSingleEntityProject(config);
             
+            _testOutputHelper.WriteLine("=xx============= TEST BEGIN. CREATING ENTITY ");
+            var e1 = await CreateSingleEntityProject(config);
+            _testOutputHelper.WriteLine("=xx============= CREATED ENTITY ");
+
             var dummy1 = await GetStepGrainById(e1, "dummy1");
             var ifNode = await GetStepGrainById(e1, "if1");
+            _testOutputHelper.WriteLine("=xx============= GOT STEPS ");
+
             using (var observer = new TestObserver(_testOutputHelper, GrainFactory))
             {
                 await observer.ObserverStep(ifNode, "if1");
@@ -54,9 +68,12 @@ namespace Zezo.Core.Grains.Tests
                 
                 Assert.Equal(StepStatus.Inactive, await ifNode.GetStatus());
                 Assert.Equal(StepStatus.Inactive, await dummy1.GetStatus());
-            
+                _testOutputHelper.WriteLine("=xx============= BEFORE KICK OFF ");
+
                 // kick off
                 await e1.Start();
+                _testOutputHelper.WriteLine("=xx============= AFTER KICK OFF ");
+
 
                 await observer.WaitUntilStatus("if1", s => s == StepStatus.Completed);
                 Assert.Equal(StepStatus.Completed, await dummy1.GetStatus());
